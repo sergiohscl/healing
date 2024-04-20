81df5790005c9eb8520c3768b9f64d4e3ae1630f
Sergio <sergiohscl2@gmail.com>
2024-04-20 12:36:00 -0300

    first commit

---
+++ medico/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import DatasAbertas, Especialidades, DadosMedico
+
+admin.site.register(Especialidades)
+admin.site.register(DadosMedico)
+admin.site.register(DatasAbertas)
